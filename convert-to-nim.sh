d29f94105de7fe53582c22a9d4cc01f30f0c5f42
Jaremy Creechley <creechley@gmail.com>
2020-04-21 22:38:09 -0600

    convert script

--- convert-to-nim.sh
+++ convert-to-nim.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-for i in $(find src -name "*.c"); do
-	echo c2nim $i;
-	c2nim --cdecl $i --out:${i:r}.nim;
+for i in $(find src -name '*.c'); do
+	f="${i%.*}"
+	echo c2nim "$i => $f"
+	c2nim --cdecl $i --out:$f.nim;
 done 
 
